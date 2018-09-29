464da87c5434479da5fcf787a1fca5166acb7a03
hongkunyoo <hongkunyoo@gmail.com>
2018-09-29 11:19:18 +0000

    change google analytics setting

--- startup.sh
+++ startup.sh
@@ -1,1 +1,2 @@
-JEKYLL_ENV=production bundle exec jekyll serve --port 5000 --drafts
+#JEKYLL_ENV=production bundle exec jekyll serve --port 5000 --drafts
+bundle exec jekyll serve --port 5000 --drafts
